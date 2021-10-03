be00ffcfc1cae7f5580d380cd4f376227100af97
nileshlad09 <89249015+nileshlad09@users.noreply.github.com>
2021-10-03 22:57:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,12 +5,17 @@
     idcount.innerText =count
 }
 
-let a=0
+
 let idsave=document.getElementById("save")
 function save(){
    let countStr= count + " - "
    idsave.textContent += countStr
    idcount.innerText=0
-   a=a+1
+   count=0
+}
+
+let idreset=document.getElementById("reset")
+function reset(){
+   idcount.innerText=0
    count=0
 }
